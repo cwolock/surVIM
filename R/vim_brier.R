ef8b77bc71104a3fd4cf9b15f905ecb2c48f0707
cwolock <cwolock@uw.edu>
2022-08-14 15:28:11 -0700

    updated brier for difference nuisances

--- R/vim_brier.R
+++ R/vim_brier.R
@@ -3,7 +3,8 @@ vim_brier <- function(time,
                       X,
                       X_reduced,
                       approx_times,
-                      landmark_times){
+                      landmark_times,
+                      nuisance){
   event.SL.library <- cens.SL.library <- c("survSL.km",
                                            "survSL.coxph",
                                            "survSL.expreg",
@@ -23,39 +24,91 @@ vim_brier <- function(time,
   # also there are weird namespace issues with predict method
   # also why is new.times a mandatory argument
   # currently we assume approx_times include landmark times
-  fit <- survSuperLearner::survSuperLearner(time = time,
-                                            event = event,
-                                            X = X,
-                                            newX = X,
-                                            new.times = c(1),
-                                            event.SL.library = event.SL.library,
-                                            cens.SL.library = cens.SL.library,
-                                            verbose = FALSE,
-                                            obsWeights = NULL,
-                                            control = list(initWeightAlg = "survSL.rfsrc"))
-  # oracle prediction, as well as nuisance predictions
-  f_hat <- 1-survSuperLearner:::predict.survSuperLearner(fit,
-                                                       newdata = X,
-                                                       new.times = landmark_times)$event.SL.predict
-  nuis_preds <- survSuperLearner:::predict.survSuperLearner(fit,
-                                                            newdata = X,
-                                                            new.times = approx_times)
-  S_hat <- nuis_preds$event.SL.predict
-  G_hat <- nuis_preds$cens.SL.predict
-  fit_reduced <- survSuperLearner::survSuperLearner(time = time,
-                                            event = event,
-                                            X = X_reduced,
-                                            newX = X_reduced,
-                                            new.times = c(1),
-                                            event.SL.library = event.SL.library,
-                                            cens.SL.library = cens.SL.library,
-                                            verbose = FALSE,
-                                            obsWeights = NULL,
-                                            control = list(initWeightAlg = "survSL.rfsrc"))
-  # reduced oracle predictions
-  fs_hat <- 1-survSuperLearner:::predict.survSuperLearner(fit_reduced,
-                                                        newdata = X_reduced,
-                                                        new.times = landmark_times)$event.SL.predict
+
+  if (nuisance == "survSL"){
+    fit <- survSuperLearner::survSuperLearner(time = time,
+                                              event = event,
+                                              X = X,
+                                              newX = X,
+                                              new.times = c(1),
+                                              event.SL.library = event.SL.library,
+                                              cens.SL.library = cens.SL.library,
+                                              verbose = FALSE,
+                                              obsWeights = NULL,
+                                              control = list(initWeightAlg = "survSL.rfsrc"))
+    # oracle prediction, as well as nuisance predictions
+    f_hat <- survSuperLearner:::predict.survSuperLearner(fit,
+                                                         newdata = X,
+                                                         new.times = landmark_times)$event.SL.predict
+    nuis_preds <- survSuperLearner:::predict.survSuperLearner(fit,
+                                                              newdata = X,
+                                                              new.times = approx_times)
+    S_hat <- nuis_preds$event.SL.predict
+    G_hat <- nuis_preds$cens.SL.predict
+    fit_reduced <- survSuperLearner::survSuperLearner(time = time,
+                                                      event = event,
+                                                      X = X_reduced,
+                                                      newX = X_reduced,
+                                                      new.times = c(1),
+                                                      event.SL.library = event.SL.library,
+                                                      cens.SL.library = cens.SL.library,
+                                                      verbose = FALSE,
+                                                      obsWeights = NULL,
+                                                      control = list(initWeightAlg = "survSL.rfsrc"))
+    # reduced oracle predictions
+    fs_hat <- survSuperLearner:::predict.survSuperLearner(fit_reduced,
+                                                          newdata = X_reduced,
+                                                          new.times = landmark_times)$event.SL.predict
+  } else if (nuisance == "param_AFT"){
+    df_full <- data.frame(time = time,
+                          event = event,
+                          X)
+    df_reduced <- data.frame(time = time,
+                             event = event,
+                             X_reduced)
+    event_fit <- survival::survreg(survival::Surv(time, event) ~ ., data = df_full,
+                                   dist = "lognormal")
+    cens_fit <- survival::survreg(survival::Surv(time, 1-event) ~ ., data = df_full,
+                                  dist = "lognormal")
+    event_fit_reduced <- survival::survreg(survival::Surv(time, event) ~ ., data = df_reduced,
+                                           dist = "lognormal")
+    event_q_pred <- predict(event_fit,
+                            newdata = X,
+                            type = 'quantile', p = seq(0, .999, by=.001))
+    cens_q_pred <- predict(cens_fit,
+                           newdata = X,
+                           type = 'quantile', p = seq(0, .999, by=.001))
+    event_q_pred_reduced <- predict(event_fit_reduced,
+                                    newdata = X_reduced,
+                                    type = 'quantile', p = seq(0, .999, by=.001))
+    # this is here to handle exact 0 times - see survSuperLearner code
+    pos.pred <- rep(1, nrow(X))
+    f_hat <- try(t(sapply(1:nrow(event_q_pred), function(j) {
+      pos.pred[j] * (1-stats::approx(event_q_pred[j,], seq(0, .999, by=.001),
+                                     xout = landmark_times,
+                                     method = 'linear',
+                                     rule = 2)$y)
+    })), silent = TRUE)
+    fs_hat <- try(t(sapply(1:nrow(event_q_pred_reduced), function(j) {
+      pos.pred[j] * (1-stats::approx(event_q_pred_reduced[j,], seq(0, .999, by=.001),
+                                     xout = landmark_times,
+                                     method = 'linear',
+                                     rule = 2)$y)
+    })), silent = TRUE)
+    S_hat <- try(t(sapply(1:nrow(event_q_pred), function(j) {
+      pos.pred[j] * (1-stats::approx(event_q_pred[j,], seq(0, .999, by=.001),
+                                     xout = approx_times,
+                                     method = 'linear',
+                                     rule = 2)$y)
+    })), silent = TRUE)
+    G_hat <- try(t(sapply(1:nrow(cens_q_pred), function(j) {
+      pos.pred[j] * (1-stats::approx(cens_q_pred[j,], seq(0, .999, by=.001),
+                                     xout = approx_times,
+                                     method = 'linear',
+                                     rule = 2)$y)
+    })), silent = TRUE)
+  }
+
 
   n <- length(time)
   # calculate integral for KM influence function
@@ -74,12 +127,14 @@ vim_brier <- function(time,
   IF.vals <- matrix(NA, nrow=n, ncol=length(landmark_times))
   brier <- rep(NA, length(landmark_times))
   brier_plug <- rep(NA, length(landmark_times))
-
+  S_t <- rep(NA, length(landmark_times))
+  G_t <- rep(NA, length(landmark_times))
   for(i in 1:length(landmark_times)) {
     t0 <- landmark_times[i]
     k <- min(which(approx_times >= t0))
-    F_hat_k <- 1-S_hat[,k]
+    #F_hat_k <- 1-S_hat[,k]
     S_hat_k <- S_hat[,k]
+    G_hat_k <- G_hat[,k]
     f_hat_k <- f_hat[,i]
     fs_hat_k <- fs_hat[,i]
     inner.func.1 <- ifelse(time <= t0 & event == 1, 1/(S_hat_Y * G_hat_Y), 0 )
@@ -87,21 +142,24 @@ vim_brier <- function(time,
     KM.if <- -S_hat_k * ( inner.func.1 - inner.func.2)
 
     # should phi_0 and phi_0s be negative or positive...
-    phi0 <- -2 * KM.if * (f_hat_k - F_hat_k)
-    phi_tilde_0 <- -(f_hat_k - F_hat_k)^2 - mean(-(f_hat_k - F_hat_k)^2)
+    phi0 <- 2 * KM.if * (f_hat_k - S_hat_k)
+    phi_tilde_0 <- -(f_hat_k - S_hat_k)^2 - mean(-(f_hat_k - S_hat_k)^2)
 
-    phi0s <- -2 * KM.if * (fs_hat_k - F_hat_k)
-    phi_tilde_0s <- -(fs_hat_k - F_hat_k)^2 - mean(-(fs_hat_k - F_hat_k)^2)
+    phi0s <- 2 * KM.if * (fs_hat_k - S_hat_k)
+    phi_tilde_0s <- -(fs_hat_k - S_hat_k)^2 - mean(-(fs_hat_k - S_hat_k)^2)
 
     if.func <- phi0 + phi_tilde_0 - phi0s - phi_tilde_0s
 
-    brier[i] <- mean(-(f_hat_k - F_hat_k)^2) - mean(-(fs_hat_k - F_hat_k)^2) + mean(if.func)
-    brier_plug[i] <- mean(-(f_hat_k - F_hat_k)^2) - mean(-(fs_hat_k - F_hat_k)^2)
+    brier[i] <- mean(-(f_hat_k - S_hat_k)^2) - mean(-(fs_hat_k - S_hat_k)^2) + mean(if.func)
+    brier_plug[i] <- mean(-(f_hat_k - S_hat_k)^2) - mean(-(fs_hat_k - S_hat_k)^2)
     IF.vals[,i] <- if.func
+    S_t[i] <- mean(S_hat_k)
+    G_t[i] <- mean(G_hat_k)
   }
 
   return(data.frame(t = landmark_times,
                     brier = brier,
-                    brier_plug = brier_plug))#,
-                    #IF.vals = IF.vals))
+                    brier_plug = brier_plug,
+                    S_t = S_t,
+                    G_t = G_t))
 }
