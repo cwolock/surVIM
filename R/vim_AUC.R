363889c38a118209a98f7e5a550a1ed6c56163c4
cwolock <cwolock@uw.edu>
2022-09-07 23:39:19 -0700

    bit of restructuring

--- R/vim_AUC.R
+++ R/vim_AUC.R
@@ -1,128 +1,48 @@
 vim_AUC <- function(time,
-                      event,
-                      X,
-                      X_reduced,
-                      approx_times,
-                      landmark_times,
-                      f_hat,
-                      fs_hat,
-                      S_hat,
-                      G_hat,
-                      holdout){
+                    event,
+                    approx_times,
+                    landmark_times,
+                    f_hat,
+                    fs_hat,
+                    S_hat,
+                    G_hat){
 
-  dimension <- 5
-  time_holdout <- holdout$y
-  event_holdout <- holdout$delta
-  X_holdout <-holdout[,1:dimension]
-  X_reduced_holdout <- holdout[,2:dimension]
+  # time <- holdout$y
+  # event <- holdout$delta
 
-  time <- time_holdout
-  event <- event_holdout
-  n <- length(time)
-  # calculate integral for KM influence function
-  # check left vs right continuous stuff here
-  int.vals <- t(sapply(1:n, function(i) {
-    vals <- diff(1/S_hat[i,])* 1/ G_hat[i,-ncol(G_hat)]
-    if(any(approx_times[-1] > time[i])){
-      vals[approx_times[-1] > time[i]] <- 0
-    }
-    c(0,cumsum(vals))
-  }))
-  # again, check left v right continuous here, especially with right = TRUE/FALSE
-  S_hat_Y <- sapply(1:n, function(i) stepfun(approx_times, c(1,S_hat[i,]), right = FALSE)(time[i]))
-  G_hat_Y <- sapply(1:n, function(i) stepfun(approx_times, c(1,G_hat[i,]), right = TRUE)(time[i]))
-  # places to hold the value of the influence function, as well as the actual estimate
-  #IF.vals <- matrix(NA, nrow=n, ncol=length(landmark_times))
   one_step <- rep(NA, length(landmark_times))
   plug_in <- rep(NA, length(landmark_times))
   var_est <- rep(NA, length(landmark_times))
-  # bottom_one_step <- rep(NA, length(landmark_times))
-  # bottom_plug_in <- rep(NA, length(landmark_times))
-  # bottom_var_est <- rep(NA, length(landmark_times))
+  full <- rep(NA, length(landmark_times))
+  reduced <- rep(NA, length(landmark_times))
   for(i in 1:length(landmark_times)) {
     t0 <- landmark_times[i]
-    k <- min(which(approx_times >= t0))
-    S_hat_k <- S_hat[,k]
-    G_hat_k <- G_hat[,k]
-    f_hat_k <- 1- f_hat[,i] # oracle is CDF, not survival
-    #fs_hat_k <- fs_hat[,i]
-    inner.func.1 <- ifelse(time <= t0 & event == 1, 1/(S_hat_Y * G_hat_Y), 0 )
-    inner.func.2 <- int.vals[,k]
-    KM.if <- -S_hat_k * ( inner.func.1 - inner.func.2)
-
-    calc_phi_01 <- function(j){
-      fx <- f_hat_k[j]
-      varphi_x <- KM.if[j]
-      int_vals <- ifelse(f_hat_k > fx, 1, 0) * (1 - S_hat_k) - ifelse(f_hat_k < fx, 1, 0) * S_hat_k
-      int <- mean(ifelse(f_hat_k > fx, 1, 0) * (1 - S_hat_k) - ifelse(f_hat_k < fx, 1, 0) * S_hat_k)
-      return(varphi_x*int)
-    }
-
-    calc_phi_tilde_01 <- function(j){
-      fx <- f_hat_k[j]
-      Sx <- S_hat_k[j]
-      int_vals <- ifelse(f_hat_k > fx, 1, 0) * (1 - S_hat_k) * Sx + ifelse(f_hat_k < fx, 1, 0) * S_hat_k * (1 - Sx)
-      int <- mean(ifelse(f_hat_k > fx, 1, 0) * (1 - S_hat_k) * Sx + ifelse(f_hat_k < fx, 1, 0) * S_hat_k * (1 - Sx))
-      return(int)
-    }
-
-    phi_01 <- unlist(lapply(1:nrow(X_holdout),
-                            FUN = calc_phi_01))
-
-    phi_tilde_01_uncentered <- unlist(lapply(1:nrow(X_holdout),
-                            FUN = calc_phi_tilde_01))
-
-    phi_tilde_01 <- phi_tilde_01_uncentered - mean(phi_tilde_01_uncentered)
-
-    calc_phi_02 <- function(j){
-      varphi_x <- KM.if[j]
-      int <- mean((1 - S_hat_k) - S_hat_k)
-      return(varphi_x * int)
-    }
-
-    calc_phi_tilde_02 <- function(j){
-      Sx <- S_hat_k[j]
-      int <- mean((1 - S_hat_k) * Sx + S_hat_k * (1 - Sx))
-      return(int)
-    }
-
-    phi_02 <- unlist(lapply(1:nrow(X_holdout),
-                            FUN = calc_phi_02))
-
-    phi_tilde_02_uncentered <- unlist(lapply(1:nrow(X_holdout),
-                                             FUN = calc_phi_tilde_02))
-
-    theta_0 <- mean(S_hat_k)
-    V_2_new <- theta_0 * (1 - theta_0)
-    phi_tilde_02_new <- S_hat_k - theta_0 - 2*theta_0*S_hat_k + 2*theta_0^2
-    phi_02_new <- KM.if * (1 - 2*theta_0)
-
-
-    phi_tilde_02 <- phi_tilde_02_uncentered - mean(phi_tilde_02_uncentered)
-
-    V_1 <- mean(phi_tilde_01_uncentered)/2
-    V_2 <- mean(phi_tilde_02_uncentered)/2
-
-    # if.func <- (phi_01 + phi_tilde_01)
-    # plug_in[i] <- mean(phi_tilde_01_uncentered)
-    # one_step[i] <- mean(phi_tilde_01_uncentered) + mean(if.func)
-    # top_plug_in[i] <- V_1
-    # top_one_step[i] <- V_1 + mean(phi_01 + phi_tilde_01)
-    # top_var_est[i] <- mean((phi_01 + phi_tilde_01)^2)
-    # bottom_plug_in[i] <- V_2
-    # bottom_one_step[i] <- V_2 + mean(phi_02 + phi_tilde_02)
-    # bottom_var_est[i] <- mean((phi_02 + phi_tilde_02)^2)
-    if.func <- (phi_01 + phi_tilde_01)/V_2 - V_1/(V_2^2)*(phi_02 + phi_tilde_02)
-    plug_in[i] <- V_1/V_2
-    one_step[i] <- V_1/V_2 + mean(if.func)
-    var_est[i] <- mean(if.func^2)
+    V_0 <- estimate_AUC(time = time,
+                        event = event,
+                        approx_times = approx_times,
+                        t0 = t0,
+                        preds = f_hat[,i],
+                        S_hat = S_hat,
+                        G_hat = G_hat)
+    V_0s <- estimate_AUC(time = time,
+                         event = event,
+                         approx_times = approx_times,
+                         t0 = t0,
+                         preds = fs_hat[,i],
+                         S_hat = S_hat,
+                         G_hat = G_hat)
+    full[i] <- V_0$one_step
+    reduced[i] <- V_0s$one_step
+    one_step[i] <- V_0$one_step - V_0s$one_step
+    plug_in[i] <- V_0$plug_in - V_0s$plug_in
+    if_func <- V_0$if_func - V_0s$if_func
+    var_est[i] <- mean(if_func^2)
   }
 
   return(data.frame(t = landmark_times,
+                    full = full,
+                    reduced = reduced,
                     one_step = one_step,
                     plug_in = plug_in,
                     var_est = var_est))
-                    # bottom_one_step = bottom_one_step,
-                    # bottom_plug_in = bottom_plug_in,
-                    # bottom_var_est = bottom_var_est))
 }
--- R/vim_brier.R
+++ R/vim_brier.R
@@ -1,90 +1,44 @@
 vim_brier <- function(time,
                       event,
-                      X,
-                      X_reduced,
                       approx_times,
                       landmark_times,
                       f_hat,
                       fs_hat,
                       S_hat,
-                      G_hat,
-                      holdout){
+                      G_hat){
 
-  # NOTE: Ted's function breaks with just a single covariate
-  # also there are weird namespace issues with predict method
-  # also why is new.times a mandatory argument
-  dimension <- 5
-  time_holdout <- holdout$y
-  event_holdout <- holdout$delta
-  X_holdout <-holdout[,1:dimension]
-  X_reduced_holdout <- holdout[,2:dimension]
-
-
-  time <- time_holdout
-  event <- event_holdout
-  n <- length(time)
-  # calculate integral for KM influence function
-  # check left vs right continuous stuff here
-  int.vals <- t(sapply(1:n, function(i) {
-    vals <- diff(1/S_hat[i,])* 1/ G_hat[i,-ncol(G_hat)]
-    if(any(approx_times[-1] > time[i])){
-      vals[approx_times[-1] > time[i]] <- 0
-    }
-    c(0,cumsum(vals))
-  }))
-  # again, check left v right continuous here, especially with right = TRUE/FALSE
-  S_hat_Y <- sapply(1:n, function(i) stepfun(approx_times, c(1,S_hat[i,]), right = FALSE)(time[i]))
-  G_hat_Y <- sapply(1:n, function(i) stepfun(approx_times, c(1,G_hat[i,]), right = TRUE)(time[i]))
-  # places to hold the value of the influence function, as well as the actual estimate
   one_step <- rep(NA, length(landmark_times))
   plug_in <- rep(NA, length(landmark_times))
   var_est <- rep(NA, length(landmark_times))
-
+  full <- rep(NA, length(landmark_times))
+  reduced <- rep(NA, length(landmark_times))
   for(i in 1:length(landmark_times)) {
     t0 <- landmark_times[i]
-    k <- min(which(approx_times >= t0))
-    S_hat_k <- S_hat[,k]
-    G_hat_k <- G_hat[,k]
-    f_hat_k <- f_hat[,i]
-    #fs_hat_k <- fs_hat[,i]
-    inner.func.1 <- ifelse(time <= t0 & event == 1, 1/(S_hat_Y * G_hat_Y), 0 )
-    inner.func.2 <- int.vals[,k]
-    KM.if <- -S_hat_k * ( inner.func.1 - inner.func.2)
-
-    # should phi_0 and phi_0s be negative or positive...
-    # phi0_old <- 2 * KM.if * (f_hat_k - S_hat_k)
-    # phi_tilde_0_old <- -(f_hat_k - S_hat_k)^2 - mean(-(f_hat_k - S_hat_k)^2)
-    #
-    # phi0s_old <- 2 * KM.if * (fs_hat_k - S_hat_k)
-    # phi_tilde_0s_old <- -(fs_hat_k - S_hat_k)^2 - mean(-(fs_hat_k - S_hat_k)^2)
-
-    phi0 <- 2*f_hat_k*KM.if - KM.if
-    phi_tilde_0 <- 2*f_hat_k*S_hat_k - f_hat_k^2 - S_hat_k - mean(2*f_hat_k*S_hat_k - f_hat_k^2 - S_hat_k)
-    # phi0s <- 2*fs_hat_k*KM.if - KM.if
-    # phi_tilde_0s <- 2*fs_hat_k*S_hat_k - fs_hat_k^2 - S_hat_k
-
-    # eta0 <- -mean(S_hat_k*(1 - S_hat_k))
-    # eta0_if_tilde <- -S_hat_k*(1 - S_hat_k) - mean(-S_hat_k*(1 - S_hat_k))
-    # eta0_if <- -KM.if + 2*KM.if*S_hat_k
-
-    if.func <- phi0 + phi_tilde_0# - phi0s - phi_tilde_0s
-    # if.func_old <- phi0_old + phi_tilde_0_old - phi0s_old - phi_tilde_0s_old
-    # if.func_eta <- eta0_if_tilde + eta0_if
-    # mu <- mean(if.func)
-    # sigma <- sd(if.func)
-    # if.func.z <- (if.func-mu)/sigma
-    # if.func <- if.func[if.func.z > -4 & if.func.z < 4]
-    # print(sum(if.func.z < -5 | if.func.z > 5)/length(if.func.z)*100)
-
-    one_step[i] <- mean(2*f_hat_k*S_hat_k - f_hat_k^2 - S_hat_k) + mean(if.func)
-    plug_in[i] <- mean(2*f_hat_k*S_hat_k - f_hat_k^2 - S_hat_k)
-    # brier_old[i] <- mean(-(f_hat_k - S_hat_k)^2) - mean(-(fs_hat_k - S_hat_k)^2) + mean(if.func_old)
-    # brier_plug_old[i] <- mean(-(f_hat_k - S_hat_k)^2) - mean(-(fs_hat_k - S_hat_k)^2)
-    #IF.vals[,i] <- if.func
-    var_est[i] <- mean(if.func^2)#var(if.func)
+    V_0 <- estimate_brier(time = time,
+                          event = event,
+                          approx_times = approx_times,
+                          t0 = t0,
+                          preds = f_hat[,i],
+                          S_hat = S_hat,
+                          G_hat = G_hat)
+    V_0s <- estimate_brier(time = time,
+                          event = event,
+                          approx_times = approx_times,
+                          t0 = t0,
+                          preds = fs_hat[,i],
+                          S_hat = S_hat,
+                          G_hat = G_hat)
+    full[i] <- V_0$one_step
+    reduced[i] <- V_0s$one_step
+    one_step[i] <- V_0$one_step - V_0s$one_step
+    plug_in[i] <- V_0$plug_in - V_0s$plug_in
+    if_func <- V_0$if_func - V_0s$if_func
+    var_est[i] <- mean(if_func^2)
   }
 
   return(data.frame(t = landmark_times,
+                    full = full,
+                    reduced = reduced,
                     one_step = one_step,
                     plug_in = plug_in,
                     var_est = var_est))
