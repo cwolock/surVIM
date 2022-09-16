5a22095a142e91a67f43c2c139ed9e877377d776
cwolock <cwolock@uw.edu>
2022-09-16 12:59:47 -0700

    sample splitting implemented

--- R/vim_AUC.R
+++ R/vim_AUC.R
@@ -6,7 +6,8 @@ vim_AUC <- function(time,
                     fs_hat,
                     S_hat,
                     G_hat,
-                    folds){
+                    folds,
+                    sample_split){
 
   one_step <- rep(NA, length(landmark_times))
   plug_in <- rep(NA, length(landmark_times))
@@ -20,6 +21,12 @@ vim_AUC <- function(time,
     CV_one_steps <- rep(NA, length(unique(folds)))
     CV_plug_ins <- rep(NA, length(unique(folds)))
     CV_var_ests <- rep(NA, length(unique(folds)))
+    split_one_step_fulls <- rep(NA, length(unique(folds)))
+    split_plug_in_fulls <- rep(NA, length(unique(folds)))
+    split_one_step_reduceds <- rep(NA, length(unique(folds)))
+    split_plug_in_reduceds <- rep(NA, length(unique(folds)))
+    split_var_est_fulls <- rep(NA, length(unique(folds)))
+    split_var_est_reduceds <- rep(NA, length(unique(folds)))
     for (j in 1:length(unique(folds))){
       time_holdout <- time[folds == j]
       event_holdout <- event[folds == j]
@@ -41,17 +48,31 @@ vim_AUC <- function(time,
       CV_reduceds[j] <- V_0s$one_step
       CV_one_steps[j] <-  V_0$one_step -V_0s$one_step
       CV_plug_ins[j] <-  V_0$plug_in -V_0s$plug_in
+      split_one_step_fulls[j] <- V_0$one_step
+      split_plug_in_fulls[j] <- V_0$plug_in
+      split_one_step_reduceds[j] <- V_0s$one_step
+      split_plug_in_reduceds[j] <- V_0s$plug_in
+      split_var_est_fulls[j] <- mean(V_0$if_func^2)
+      split_var_est_reduceds[j] <- mean(V_0s$if_func^2)
       if_func <- V_0$if_func - V_0s$if_func
       CV_var_ests[j] <- mean(if_func^2)
     }
 
 
-    full[i] <- mean(CV_fulls)#V_0$one_step
-    reduced[i] <- mean(CV_reduceds)#V_0s$one_step
-    one_step[i] <- mean(CV_one_steps)#V_0$one_step - V_0s$one_step
-    plug_in[i] <- mean(CV_plug_ins)#V_0$plug_in - V_0s$plug_in
-    #if_func <- V_0$if_func - V_0s$if_func
-    var_est[i] <- mean(CV_var_ests)#mean(if_func^2)
+    if (sample_split){
+      ss_folds <- sample(rep(1:2, length = length(unique(folds))))
+      one_step[i] <- mean(split_one_step_fulls[ss_folds == 1]) - mean(split_one_step_reduceds[ss_folds == 2])
+      plug_in[i] <- mean(split_plug_in_fulls[ss_folds == 1]) - mean(split_plug_in_reduceds[ss_folds == 2])
+      full[i] <- mean(split_one_step_fulls[ss_folds == 1])
+      reduced[i] <- mean(split_one_step_reduceds[ss_folds == 2])
+      var_est[i] <- mean(split_var_est_fulls[ss_folds == 1]) + mean(split_var_est_fulls[ss_folds == 2])
+    } else{
+      one_step[i] <- mean(CV_one_steps)
+      plug_in[i] <- mean(CV_plug_ins)
+      var_est[i] <- mean(CV_var_ests)
+      full[i] <- mean(CV_fulls)#V_0$one_step
+      reduced[i] <- mean(CV_reduceds)#V_0s$one_step
+    }
   }
 
   return(data.frame(t = landmark_times,
