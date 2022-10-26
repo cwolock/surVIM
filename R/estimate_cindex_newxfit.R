e77cfb8f84773e04edc198bfcbc12fa5d68fde38
cwolock <cwolock@uw.edu>
2022-10-26 09:47:47 -0700

    changed integration in c index

--- R/estimate_cindex_newxfit.R
+++ R/estimate_cindex_newxfit.R
@@ -1,12 +1,12 @@
 estimate_cindex_newxfit <- function(time,
-                            event,
-                            approx_times,
-                            preds,
-                            S_hat,
-                            G_hat,
-                            tau,
-                            preds_holdout,
-                            S_hat_holdout){
+                                    event,
+                                    approx_times,
+                                    preds,
+                                    S_hat,
+                                    G_hat,
+                                    tau,
+                                    preds_holdout,
+                                    S_hat_holdout){
 
   n <- length(time)
   # calculate integral for KM influence function
@@ -36,49 +36,56 @@ estimate_cindex_newxfit <- function(time,
   KM_ifs <- matrix(unlist(lapply(1:length(approx_times), FUN = calc_KM_if)),
                    nrow = n)
 
-  approx_times_k <- approx_times[approx_times <= tau]
-  approx_times_k <- approx_times_k[-1]
+  # approx_times_k <- approx_times[approx_times <= tau]
+  # approx_times_k <- approx_times_k[-1]
   S_hat_k <- S_hat[,approx_times <= tau]
   S_hat_k_holdout <- S_hat_holdout[,approx_times <= tau]
-  #G_hat_k <- G_hat[,approx_times <= tau]
   KM_ifs_k <- KM_ifs[,approx_times <= tau]
+
+  k <- length(approx_times)
 
   calc_phi_01 <- function(j){
     fx <- preds[j]
     varphi_x <- KM_ifs_k[j,]
     exceed_probs1 <- -rowSums(sweep(S_hat_k_holdout[,-1], MARGIN=2, diff(varphi_x), `*`))
     exceed_probs2 <- -rowSums(sweep(t(diff(t(S_hat_k_holdout))), MARGIN=2, varphi_x[-1], `*`))
-    # int <- -mean(ifelse(fx > preds, 1, 0) * rowSums(S_hat_k[,-1] * diff(varphi_x)) +
-    #                ifelse(preds > fx, 1, 0) *  rowSums(varphi_x[-1] * t(diff(t(S_hat_k)))))
-    #int <- mean(ifelse(fx > preds, 1, 0) * exceed_probs - ifelse(preds > fx, 1, 0) * exceed_probs) # old version
-    int <- mean(ifelse(fx > preds_holdout, 1, 0) * exceed_probs1 + ifelse(preds_holdout > fx, 1, 0) * exceed_probs2)
+    int <- mean(ifelse(fx > preds_holdout, 1, 0) * exceed_probs1 + ifelse(preds_holdout >= fx, 1, 0) * exceed_probs2)
     return(int)
   }
 
-  phi_01 <- unlist(lapply(1:n, FUN = calc_phi_01))
+  calc_phi_01_new <- function(j){
+    fx <- preds[j]
+    varphi_x <- KM_ifs_k[j,]
+    exceed_probs1 <- -rowSums(sweep(S_hat_k_holdout[,-k], MARGIN=2, diff(varphi_x), `*`))
+    exceed_probs2 <- -rowSums(sweep(t(diff(t(S_hat_k_holdout))), MARGIN=2, varphi_x[-k], `*`))
+    int <- mean(ifelse(fx > preds_holdout, 1, 0) * exceed_probs1 + ifelse(preds_holdout >= fx, 1, 0) * exceed_probs2)
+    return(int)
+  }
+
+  # phi_01 <- unlist(lapply(1:n, FUN = calc_phi_01))
+  phi_01_new <- unlist(lapply(1:n, FUN = calc_phi_01_new))
 
   calc_phi_tilde_01 <- function(j){
-    #tx <- time[j]
-    #deltax <- event[j]
-    #l <- min(which(approx_times >= tx))
-    #wx <- G_hat_k[j,l]
     fx <- preds[j]
     Sx <- S_hat_k[j,]
-    # int1 <- mean(ifelse(fx > preds & tx < time, 1, 0) * deltax / (wx * G_hat_k[,l]))
-    # int2 <- mean(ifelse(tx < time, 1, 0) * deltax / (wx * G_hat_k[,l]))# * rowSums(S_hat_k[,-1] * diff(Sx))) #+
-    #                #ifelse(preds > fx, 1, 0) *  rowSums(Sx[-1] * t(diff(t(S_hat_k)))))
-    exceed_probs1 <- -rowSums(sweep(S_hat_k_holdout[,-1], MARGIN=2, diff(Sx), `*`)) # old one, but doesn't properly account for tau
+    exceed_probs1 <- -rowSums(sweep(S_hat_k_holdout[,-1], MARGIN=2, diff(Sx), `*`))
     exceed_probs2 <- -rowSums(sweep(t(diff(t(S_hat_k_holdout))), MARGIN=2, Sx[-1], `*`))
-    #exceed_probs <-  -rowSums(S_hat_k[,-1] * diff(Sx))
     int <- mean(ifelse(fx > preds_holdout, 1, 0) * exceed_probs1 +
-                  ifelse(preds_holdout > fx, 1, 0) * exceed_probs2)#-rowSums(Sx[-1] * t(diff(t(S_hat_k)))))
-    # -rowSums(S_hat_k[,-1] * diff(Sx))
-    # -rowSums(sweep(t(diff(t(S_hat_k))), MARGIN=2, Sx[-1], `*`))
-    # -rowSums(Sx[-1] * t(diff(t(S_hat_k))))
+                  ifelse(preds_holdout >= fx, 1, 0) * exceed_probs2)
+    return(int)
+  }
+  calc_phi_tilde_01_new <- function(j){
+    fx <- preds[j]
+    Sx <- S_hat_k[j,]
+    exceed_probs1 <- -rowSums(sweep(S_hat_k_holdout[,-k], MARGIN=2, diff(Sx), `*`))
+    exceed_probs2 <- -rowSums(sweep(t(diff(t(S_hat_k_holdout))), MARGIN=2, Sx[-k], `*`))
+    int <- mean(ifelse(fx > preds_holdout, 1, 0) * exceed_probs1 +
+                  ifelse(preds_holdout >= fx, 1, 0) * exceed_probs2)
     return(int)
   }
 
-  phi_tilde_01_uncentered <- unlist(lapply(1:n, FUN = calc_phi_tilde_01))
+  # phi_tilde_01_uncentered <- unlist(lapply(1:n, FUN = calc_phi_tilde_01))
+  phi_tilde_01_uncentered_new <- unlist(lapply(1:n, FUN = calc_phi_tilde_01_new))
 
   calc_phi_02 <- function(j){
     varphi_x <- KM_ifs_k[j,]
@@ -88,40 +95,60 @@ estimate_cindex_newxfit <- function(time,
     return(int)
   }
 
-  phi_02 <- unlist(lapply(1:n, FUN = calc_phi_02))
-
-  calc_phi_tilde_02 <- function(j){
-    #tx <- time[j]
-    #deltax <- event[j]
-    #l <- min(which(approx_times >= tx))
-    #wx <- G_hat_k[j,l]
-    Sx <- S_hat_k[j,]
-    exceed_probs1 <- -rowSums(sweep(S_hat_k_holdout[,-1], MARGIN=2, diff(Sx), `*`)) # old one, but doesn't properly account for tau
-    exceed_probs2 <- -rowSums(sweep(t(diff(t(S_hat_k_holdout))), MARGIN=2, Sx[-1], `*`))
-    #exceed_probs <-  -rowSums(S_hat_k[,-1] * diff(Sx))
-    int <- mean(exceed_probs1 + exceed_probs2)#-rowSums(Sx[-1] * t(diff(t(S_hat_k)))))
+  calc_phi_02_new <- function(j){
+    varphi_x <- KM_ifs_k[j,]
+    exceed_probs1 <- -rowSums(sweep(S_hat_k_holdout[,-k], MARGIN=2, diff(varphi_x), `*`))
+    exceed_probs2 <- -rowSums(sweep(t(diff(t(S_hat_k_holdout))), MARGIN=2, varphi_x[-k], `*`))
+    int <- mean(exceed_probs1 + exceed_probs2)
     return(int)
   }
 
-  phi_tilde_02_uncentered <- unlist(lapply(1:n, FUN = calc_phi_tilde_02))
+
+  # phi_02 <- unlist(lapply(1:n, FUN = calc_phi_02))
+  phi_02_new <- unlist(lapply(1:n, FUN = calc_phi_02_new))
+
+  calc_phi_tilde_02 <- function(j){
+    Sx <- S_hat_k[j,]
+    exceed_probs1 <- -rowSums(sweep(S_hat_k_holdout[,-1], MARGIN=2, diff(Sx), `*`))
+    exceed_probs2 <- -rowSums(sweep(t(diff(t(S_hat_k_holdout))), MARGIN=2, Sx[-1], `*`))
+    int <- mean(exceed_probs1 + exceed_probs2)
+    return(int)
+  }
+
+  calc_phi_tilde_02_new <- function(j){
+    Sx <- S_hat_k[j,]
+    exceed_probs1 <- -rowSums(sweep(S_hat_k_holdout[,-k], MARGIN=2, diff(Sx), `*`))
+    exceed_probs2 <- -rowSums(sweep(t(diff(t(S_hat_k_holdout))), MARGIN=2, Sx[-k], `*`))
+    int <- mean(exceed_probs1 + exceed_probs2)
+    return(int)
+  }
+
+  # phi_tilde_02_uncentered <- unlist(lapply(1:n, FUN = calc_phi_tilde_02))
+
+  phi_tilde_02_uncentered_new <- unlist(lapply(1:n, FUN = calc_phi_tilde_02_new))
 
 
-  phi_tilde_01 <- phi_tilde_01_uncentered - mean(phi_tilde_01_uncentered)
-  phi_tilde_02 <- phi_tilde_02_uncentered - mean(phi_tilde_02_uncentered)
-  V_1 <- mean(phi_tilde_01_uncentered)/2
-  V_2 <- mean(phi_tilde_02_uncentered)/2
-  if_func <- (phi_01 + phi_tilde_01)/V_2 - V_1/(V_2^2)*(phi_02 + phi_tilde_02)
-  plug_in <- V_1/V_2
-  one_step <- V_1/V_2 + mean(if_func)
+  # phi_tilde_01 <- phi_tilde_01_uncentered - mean(phi_tilde_01_uncentered)
+  # phi_tilde_02 <- phi_tilde_02_uncentered - mean(phi_tilde_02_uncentered)
+  phi_tilde_01_new <- phi_tilde_01_uncentered_new - mean(phi_tilde_01_uncentered_new)
+  phi_tilde_02_new <- phi_tilde_02_uncentered_new - mean(phi_tilde_02_uncentered_new)
+  # note that including phi_tilde in the correction is a bit silly because we've
+  # centered that term - it has no contribution to the correction (nor should it)
+  # if we're underestimating, (both one-step and plug-in), then that means
+  # the ratio V_1/V_2 is too small. So this gets amplfied in the one-step (we subtract
+  # off something larger, making the estimate smaller)
+  # V_1 <- mean(phi_tilde_01_uncentered)/2
+  # V_2 <- mean(phi_tilde_02_uncentered)/2
+  V_1_new <- mean(phi_tilde_01_uncentered_new)/2
+  V_2_new <- mean(phi_tilde_02_uncentered_new)/2
+  # if_func <- (phi_01 + phi_tilde_01)/V_2 - V_1/(V_2^2)*(phi_02 + phi_tilde_02)
+  # plug_in <- V_1/V_2
+  # one_step <- V_1/V_2 + mean(if_func)
 
-  # plug_in <- mean(phi_tilde_01)#/mean(phi_tilde_01_b)
-  # # UnoC(Surv(time, event), Surv(dtest$time, dtest$event), lpnew = preds)
-  #
-  # phi_tilde_01 <- phi_tilde_01 - plug_in
-  #
-  # if_func <- phi_01 + phi_tilde_01
-  #
-  # one_step <- plug_in + mean(if_func)
+
+  if_func <- (phi_01_new + phi_tilde_01_new)/V_2_new - V_1_new/(V_2_new^2)*(phi_02_new + phi_tilde_02_new)
+  plug_in <- V_1_new/V_2_new
+  one_step <- V_1_new/V_2_new + mean(if_func_new)
 
   return(list(one_step = one_step,
               plug_in = plug_in,
